fd059dabd64cf87ebc2e698b83a6dd8b5db9711e
Khayyam Huseynli <53341322+khayyam-huseynli@users.noreply.github.com>
2024-08-22 15:55:25 +0400

    Updated typing-hints

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,9 +6,11 @@
                  numbers.
 '''
 
+from typing import List
+
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     ''' Return list of values yielded by async_generator. '''
     return [i async for i in async_generator()]
