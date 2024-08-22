5321a6e7d0cc53962b9aed46603536f320909b94
Khayyam Huseynli <53341322+khayyam-huseynli@users.noreply.github.com>
2024-08-22 15:53:16 +0400

    bug fixed

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,10 +8,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     ''' Generator that yields a random value between 0 and 10 every second,
         10 times. '''
     for i in range(10):
